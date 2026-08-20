a32965d17f6b7dc12668b0881b613660fe62484d
rpdoody <rdoody@cox.net>
2026-08-20 12:42:27 +0000

    Refactor 4_Models.py to add spacing for improved readability in the initialization section.

--- pages/4_Models.py
+++ pages/4_Models.py
@@ -24,9 +24,11 @@ min_year, max_year = render_sidebar()
 _pre = _load_precomputed()
 
 _GAMEINFO_MAX_YEAR = int(_pre["gameinfo"]["season"].max())
+
 features_df = _pre["model_features"][
     _pre["model_features"]["season"].between(min_year, max_year)
 ].copy()
+
 init_session_state(features_df=features_df)
 
 
