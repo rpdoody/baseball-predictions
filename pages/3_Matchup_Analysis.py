2179f24c2639d7f2a920b92516b4bc71863102f4
rpdoody <rdoody@cox.net>
2026-08-22 01:48:39 +0000

    feat: update season range selection to use select_slider for improved user interaction

--- pages/3_Matchup_Analysis.py
+++ pages/3_Matchup_Analysis.py
@@ -24,7 +24,12 @@ from retrosheet import TEAM_NAMES, head_to_head, rolling_team_form
 
 current_year = datetime.datetime.now(ET).year
 
-min_year, max_year = render_sidebar()
+min_year, max_year = st.select_slider(
+    "Season range",
+    options=list(range(2020, current_year + 1)),
+    value=(2020, current_year),
+    key="season_range_on_page",
+)
 
 _pre = _load_precomputed()
 _gi = _pre["gameinfo"][_pre["gameinfo"]["season"].between(min_year, max_year)].copy()
